a8c085b3db1d76fe172341716fdc7808614b3ee1
Snat <matthew.ellis@ieg.ac.uk>
2025-10-05 15:27:03 +0100

    Add initial implementation of conditional statements in if.js

---
+++ js/variables/conditions/if.js
@@ -0,0 +1,9 @@
+let number = 10;
+
+if (number > 20) {
+    console.log("Number is greater than 20");
+} else if (number > 5) {
+    console.log("Number is greater than 5 but less than or equal to 20");
+} else {
+    console.log("Number is 5 or less");
+}
---
+++ js/variables/conditions/switch.js
@@ -0,0 +1,15 @@
+const fruit = 'apple';
+
+switch (fruit) {
+    case 'apple':
+        console.log('You chose apple.');
+        break;
+    case 'banana':
+        console.log('You chose banana.');
+        break;
+    case 'orange':
+        console.log('You chose orange.');
+        break;
+    default:
+        console.log('Unknown fruit.');
+}
